40b466b33dc78db54815c5ee0cfe00227723d591
Ashot Barkhudaryan <ashot7410@gmail.com>
2021-05-06 02:37:05 +0400

    Project Organization

--- scripts/build.py
+++ scripts/build.py
@@ -1,7 +1,8 @@
 import os
 # create build folder if needed
-if os.path.isdir('build'):	
+if os.path.isdir('../build'):	
 	os.system('mkdir build')
 
+os.system('cd ..')
 os.system('cmake -S . -B build -G Ninja')
 os.system('cmake --build build')
